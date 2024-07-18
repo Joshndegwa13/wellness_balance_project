14c66eb4ca3583091d1b38105903573c90405978
Joshndegwa13 <ndegwajosh7@gmail.com>
2024-07-18 22:30:41 +0300

    update changes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById('fitness-form').addEventListener('submit', function (eve
     const level = document.getElementById('level').value;
 
     // Fetch the workout plans from the JSON server raw data 
-    fetch('http://localhost:3000/workoutPlans')
+    fetch('https://wellness-balance-project.vercel.app/')
         .then(response => response.json()) // Parse the JSON data from the response
         .then(data => {
             // Finding the workout plan that matches the selected goal and level
